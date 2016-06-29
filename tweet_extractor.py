9a99070db19753ad923a287f3edb88f51b6ed472
Mitsuzawa <amitsuzawa@SDGL140e6a947.local>
2016-06-29 16:12:25 -0700

    Pull data every 15 minutes

--- tweet_extractor.py
+++ tweet_extractor.py
@@ -1,6 +1,11 @@
 from twitter import *
 import pandas as pd
 import codecs
+import os
+from datetime import datetime
+import time
+
+starttime=time.time()
 
 t = Twitter(auth=OAuth(consumer_key, consumer_secret, access_token_key, access_token_secret))
 
@@ -14,6 +19,10 @@ def write_tweets(dest_file, dictionary=portuguese_dict):
     for word in dictionary:
         result += search_by_term(word)
     df = pd.DataFrame(result)
-    df.to_csv(dest_file, columns=['text'], index_label='search_trans_id', encoding='utf-8')
-    
-write_tweets(open('tweets.csv','w'))
+    print 'TOTAL NUMBER OF TWEETS: ', len(df)
+    print 'TIME: ', str(datetime.now())
+    df.to_csv(dest_file, columns=['text', 'id’, ’created_at’], encoding='utf-8')
+
+while True:
+    write_tweets('portuguese_input'+ 'tweets' + str(time.time()) + '.csv')
+    time.sleep(900.0 - ((time.time() - starttime) % 900.0))
